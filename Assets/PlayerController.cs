ade53ed36533b92e929751f3b03288f0f314ae00
Paulhughes2 <plhgh118@hotmail.com>
2018-05-31 14:01:40 +0100

    fxcvf

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : NetworkBehaviour
 
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-		Debug.Log("test");
+		
 
 		transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
